4bbc8e178fea3ea372b3a10424533bb5f822620d
magical-flow-3186 <rregalado17@gmail.com>
2019-06-03 21:12:35 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,13 @@
+def instantiate_new_array
+  @my_new_array = []
+end
+
+def array_with_two_elements
+  @my_two_array = ["",""]
+end
+
+
+def first_element
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]  
+  taylor_swift[0]
+end
