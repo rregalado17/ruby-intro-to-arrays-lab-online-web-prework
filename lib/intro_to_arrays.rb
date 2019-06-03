f8fb1b76c0f6e9ecb2bbad62212d79a6c66e0392
magical-flow-3186 <rregalado17@gmail.com>
2019-06-03 21:14:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,8 +6,9 @@ def array_with_two_elements
   @my_two_array = ["",""]
 end
 
+@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]  
 
-def first_element
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]  
+def first_element()
+
   taylor_swift[0]
 end
