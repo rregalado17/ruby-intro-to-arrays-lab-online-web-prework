0d62aa94ef3b0006e72b31352df91d87668ef38f
magical-flow-3186 <rregalado17@gmail.com>
2019-06-03 21:15:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,7 +8,7 @@ end
 
 @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]  
 
-def first_element()
-
+def first_element(my_first_element)
+  my_first_element = @taylor_swift
   taylor_swift[0]
 end
